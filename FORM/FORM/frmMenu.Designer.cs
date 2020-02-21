99eb4e9e93b24f0acf1fcd3cff97a8ccff6c3d4b
Vanderson da Silva Quintino <46028991+vandersonsq@users.noreply.github.com>
2020-02-21 12:09:54 -0300

    arquivos atualizados (com sons funcionando)

---
+++ FORM/FORM/frmMenu.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FORM
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btPlay = new System.Windows.Forms.Button();
+            this.btPause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btCalcular.BackColor = System.Drawing.Color.Gray;
+            this.btCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btCalcular.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.ForeColor = System.Drawing.Color.White;
+            this.btCalcular.Location = new System.Drawing.Point(378, 138);
+            this.btCalcular.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(288, 72);
+            this.btCalcular.TabIndex = 22;
+            this.btCalcular.Text = "Adição";
+            this.btCalcular.UseVisualStyleBackColor = false;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(378, 240);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 72);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Multiplicação";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(378, 348);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(288, 72);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Subtração";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.BackColor = System.Drawing.Color.Gray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(378, 458);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(288, 72);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Divisão";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btPlay
+            // 
+            this.btPlay.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btPlay.BackColor = System.Drawing.Color.White;
+            this.btPlay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btPlay.Image = ((System.Drawing.Image)(resources.GetObject("btPlay.Image")));
+            this.btPlay.Location = new System.Drawing.Point(872, 557);
+            this.btPlay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btPlay.Name = "btPlay";
+            this.btPlay.Size = new System.Drawing.Size(70, 66);
+            this.btPlay.TabIndex = 26;
+            this.btPlay.UseVisualStyleBackColor = false;
+            this.btPlay.Click += new System.EventHandler(this.btPlay_Click);
+            // 
+            // btPause
+            // 
+            this.btPause.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btPause.BackColor = System.Drawing.Color.White;
+            this.btPause.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btPause.Image = ((System.Drawing.Image)(resources.GetObject("btPause.Image")));
+            this.btPause.Location = new System.Drawing.Point(951, 557);
+            this.btPause.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btPause.Name = "btPause";
+            this.btPause.Size = new System.Drawing.Size(70, 66);
+            this.btPause.TabIndex = 27;
+            this.btPause.UseVisualStyleBackColor = false;
+            this.btPause.Click += new System.EventHandler(this.btPause_Click);
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 628);
+            this.Controls.Add(this.btPause);
+            this.Controls.Add(this.btPlay);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMenu";
+            this.Padding = new System.Windows.Forms.Padding(30, 92, 30, 31);
+            this.Text = "Menu de Operações";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMenu_FormClosed);
+            this.Load += new System.EventHandler(this.frmMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btPlay;
+        private System.Windows.Forms.Button btPause;
+    }
+}
---
+++ FORM/FORM/frmMenu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FORM
+{
+    public partial class frmMenu : MetroFramework.Forms.MetroForm
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            frmAdicao adicao = new frmAdicao();
+            adicao.Show();
+            this.Hide();
+            SoundPlayer somMusica = new SoundPlayer(@"C:\Users\Vanderson\Desktop\FORM\Audios\cafe.wav");
+            somMusica.Play();
+            somMusica.Stop();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmMultiplicacao mult = new frmMultiplicacao();
+            mult.Show();
+            this.Hide();
+            SoundPlayer somMusica = new SoundPlayer(@"C:\Users\Vanderson\Desktop\FORM\Audios\cafe.wav");
+            somMusica.Play();
+            somMusica.Stop();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmSubtracao sub = new frmSubtracao();
+            sub.Show();
+            this.Hide();
+            SoundPlayer somMusica = new SoundPlayer(@"C:\Users\Vanderson\Desktop\FORM\Audios\cafe.wav");
+            somMusica.Play();
+            somMusica.Stop();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmDivisao div = new frmDivisao();
+            div.Show();
+            this.Hide();
+            SoundPlayer somMusica = new SoundPlayer(@"C:\Users\Vanderson\Desktop\FORM\Audios\cafe.wav");
+            somMusica.Play();
+            somMusica.Stop();
+        }
+        private void tocarMusicaMenu()
+        {
+            SoundPlayer somMusica = new SoundPlayer(@"\FORM\Audios\cafe.wav");
+            somMusica.Play();
+        }
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+            tocarMusicaMenu();
+        }
+        private void btPlay_Click(object sender, EventArgs e)
+        {
+            tocarMusicaMenu();
+        }
+        private void btPause_Click(object sender, EventArgs e)
+        {
+            tocarMusicaMenu();
+        }
+        private void frmMenu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+    }
+}
